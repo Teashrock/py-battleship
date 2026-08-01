6da3106c193b79f0945afdd5c9221895896b4f66
Teashrock <kajitsu22@gmail.com>
2026-08-01 23:50:40 +0300

    Next

--- app/main.py
+++ app/main.py
@@ -59,7 +59,7 @@ class Battleship:
             elif start[1] != end[1]:
                 present_points = [(start[0], y) for y in range(start[1], end[1] + 1)]
             else:
-                present_points = ship[0]
+                present_points = [(ship[0])]
             self.field[tuple(present_points)] = Ship(ship[0], ship[1])
         print(self.field)
 
