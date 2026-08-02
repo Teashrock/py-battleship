2798efae5dc848b615f8ff1c13f178a5bc83fad2
Teashrock <kajitsu22@gmail.com>
2026-08-02 22:00:15 +0300

    Next

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 class Deck:
-    def __init__(self, row: int, column: int, is_alive:bool=True) -> None:
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
         self.row = row
         self.column = column
         self.is_alive = is_alive
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False) -> None:
+    def __init__(
+        self,
+        start: list,
+        end: list,
+        is_drowned: bool = False
+    ) -> None:
         # Create decks and save them to a list `self.decks`
         self.decks: list[Deck] = []
         self.is_drowned = is_drowned
@@ -14,23 +19,23 @@ class Ship:
         if start == end:
             self.decks = [Deck(*start)]
         else:
-            start_row    = start[0]
+            start_row = start[0]
             start_column = start[1]
-            end_row      = end[0]
-            end_column   = end[1]
+            end_row = end[0]
+            end_column = end[1]
             self.decks = [
                 Deck(row, column)
                 for row in range(start_row, end_row + 1)
                 for column in range(start_column, end_column + 1)
             ]
 
-    def get_deck(self, row, column) -> Deck | None:
+    def get_deck(self, row: int, column: int) -> Deck | None:
         # Find the corresponding deck in the list
         for deck in self.decks:
             if row == deck.row and column == deck.column:
                 return deck
 
-    def fire(self, row, column) -> str:
+    def fire(self, row: int, column: int) -> str:
         # Change the `is_alive` status of the deck
         # And update the `is_drowned` value if it's needed
         fired_deck = self.get_deck(row, column)
@@ -48,16 +53,21 @@ class Battleship:
         # Its keys are tuples - the coordinates of the non-empty cells,
         # A value for each cell is a reference to the ship
         # which is located in it
-        #self.field: dict[tuple, Ship] = {ship: Ship(ship[0], ship[1]) for ship in ships}
         self.field: dict[tuple, Ship] = {}
         for ship in ships:
             present_points = []
             start = ship[0]
-            end   = ship[1]
+            end = ship[1]
             if start[0] != end[0]:
-                present_points = [(x, end[1]) for x in range(start[0], end[0] + 1)]
+                present_points = [
+                    (x, end[1])
+                    for x in range(start[0], end[0] + 1)
+                ]
             elif start[1] != end[1]:
-                present_points = [(start[0], y) for y in range(start[1], end[1] + 1)]
+                present_points = [
+                    (start[0], y)
+                    for y in range(start[1], end[1] + 1)
+                ]
             else:
                 present_points = [(ship[0])]
             self.field[tuple(present_points)] = Ship(ship[0], ship[1])
@@ -74,7 +84,7 @@ class Battleship:
 
     def print_field(self) -> None:
         max_column = 9
-        max_row    = 9
+        max_row = 9
         row = 0
         column = 0
         while column < max_column:
@@ -94,7 +104,7 @@ class Battleship:
             print()
             column += 1
 
-    def _validate_field(self):
+    def _validate_field(self) -> None:
         if len(self.field) != 10:
             raise ValueError("The total amount of ships should be 10!")
         deck_control = [0, 0, 0, 0]
